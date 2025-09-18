4c0272081946ebe70fb33a74edcbab9f39a62f14
jademon <j2python@gmail.com>
2025-09-18 23:13:01 +0900

    Update Practice17.java

--- Week03/2024042046/Practice17.java
+++ Week03/2024042046/Practice17.java
@@ -4,20 +4,25 @@ public class Practice17 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while(true) {
+            int min = 0;
+            int max = 99;
             int num = (int)(Math.random()*100);
             System.out.println("수를 결정하였습니다. 맞추어 보세요.");
-            System.out.println("0-99");
+            
 
             int count = 1;
             while(true) {
+                System.out.println(min+"-"+max);
                 System.out.print(count+">>");
                 int num2 = scanner.nextInt();
 
                 if(num2 == num) {
                     break;
                 } else if(num2 < num) {
+                    min = num2;
                     System.out.println("더 높게");
                 } else {
+                    max = num2;
                     System.out.println("더 낮게");
                 }
 
