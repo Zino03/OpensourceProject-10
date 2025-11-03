99d1adf11d601e5c03f5fae03dc8ff881f9406d1
github-jademon <j2python@gmail.com>
2025-11-03 16:05:24 +0900

    Feat: create backend project

---
+++ Project/Backend/src/main/java/com/example/saja_saja/SajaSajaApplication.java
@@ -0,0 +1,13 @@
+package com.example.saja_saja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SajaSajaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SajaSajaApplication.class, args);
+	}
+
+}
---
+++ Project/Backend/src/test/java/com/example/saja_saja/SajaSajaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.saja_saja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SajaSajaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
