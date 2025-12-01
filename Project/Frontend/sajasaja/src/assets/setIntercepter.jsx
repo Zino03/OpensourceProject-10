d9d55a8d447af4b30e8f2eac6e5efd3849cc8a47
github-jademon <j2python@gmail.com>
2025-12-01 18:14:12 +0900

    develop

---
+++ Project/Frontend/sajasaja/src/assets/setIntercepter.jsx
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: "http://192.168.31.28:8080", // 백엔드 주소
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export const setInterceptor = (token) => {
+  if (!token) return false;
+  api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  return true;
+};
